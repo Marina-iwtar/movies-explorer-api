47335453524907b57334a4fb6d83ee10277331de
Marina Khusainova <marina.iwtar@yandex.ru>
2023-09-17 14:48:53 +0500

    исправление корс1

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
-const BD_BITFILMS = process.env.BD_BITFILMS || 'mongodb://127.0.0.1:27017/bitfilmsdb';
+const BD_BITFILMS = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 module.exports = {
   PORT,
   BD_BITFILMS,
