62d2d88efdbe3bbff2ac807ef71313b148a0d765
Marina Khusainova <marina.iwtar@yandex.ru>
2023-09-17 13:34:17 +0500

    исправление корс1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,6 +3,8 @@ const allowedCors = [
   'http://iwtarmovies.nomoredomainsicu.ru',
   'https://localhost:4000',
   'http://localhost:4000',
+  'http://localhost:3000',
+  'https://localhost:3000',
 ];
 
 module.exports = (req, res, next) => {
